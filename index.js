dde6ada97cb21ba21e784307583ea9ee47432669
Sarthak <notsarthakshah@gmail.com>
2024-05-31 12:04:30 +0530

    add: post req

---
+++ index.js
@@ -0,0 +1,34 @@
+import express from "express";
+import OpenAI from "openai";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+app.post("/generate", async (req, res) => {
+  const { prompt } = req.body;
+
+  try {
+    const completion = await openai.completions.create({
+      model: "gpt-3.5-turbo-instruct",
+      prompt,
+      max_tokens: 8,
+      temperature: 0,
+    });
+    console.log(completion);
+
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "An error occurred during Blog Generation" });
+  }
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
