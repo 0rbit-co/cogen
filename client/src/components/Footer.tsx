3167e1581984bc81625e2ea3219587ed1f6976a8
Sarthak <notsarthakshah@gmail.com>
2024-06-02 20:39:20 +0530

    minor fixes

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -7,9 +7,9 @@ const Footer = () => {
     <div className="w-full bg-[#25291C] text-[#EB8F44] flex justify-between py-2 px-20">
       <div>&copy; 2024 0rbit</div>
       <div className="flex items-center gap-6">
-        <FaDiscord className="text-2xl"/>
-        <FaXTwitter className="text-2xl"/>
-        <FaGithub className="text-2xl"/>
+        <FaDiscord className="text-2xl cursor-pointer"/>
+        <FaXTwitter className="text-2xl cursor-pointer"/>
+        <FaGithub className="text-2xl cursor-pointer"/>
       </div>
     </div>
   )
